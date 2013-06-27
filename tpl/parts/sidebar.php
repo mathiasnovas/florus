2529fbd8b8c2c86d482ba8f07a9b4a85adb97903
Mathias Novas <mathias@07.no>
2013-06-27 15:50:11 +0200

    Design changes

--- tpl/parts/sidebar.php
+++ tpl/parts/sidebar.php
@@ -1,48 +1,48 @@
 <aside class="sidebar columns large-3">
 
-    <div class="box categories">
-        <h2>Kategorier</h2>
-        <?php
-            $categories = Florus::getCategories();
-            require_once 'tpl/parts/categories.php';
-        ?>
-    </div>
+        <div class="columns large-12 small-6 box categories">
+            <h2>Kategorier</h2>
+            <?php
+                $categories = Florus::getCategories();
+                require_once 'tpl/parts/categories.php';
+            ?>
+        </div>
 
-    <div class="box">
-        <h2>Sosialt</h2>
-        <ul>
-            <li>
-                <a href="">Facebook</a>
-            </li>
-            <li>
-                <a href="">Twitter</a>
-            </li>
-        </ul>
-    </div>
-
-    <div class="box blog">
-        <h2>Blogg</h2>
-        <ul>
-            <?php foreach(FLorus::getPosts() as $post) { ?>
+        <div class="columns large-12 small-6 box">
+            <h2>Sosialt</h2>
+            <ul>
                 <li>
-                    <a href="#"><?php echo $post['title']; ?></a>
-                    <span class="date"><?php echo $post['date']; ?></span>
+                    <a href="">Facebook</a>
                 </li>
-            <?php } ?>
-        </ul>
-    </div>
+                <li>
+                    <a href="">Twitter</a>
+                </li>
+            </ul>
+        </div>
 
-    <div class="box newsletter">
-        <h2>Nyhetsbrev</h2>
-        <form action="">
-            <input type="email" placeholder="blomster@finn.no">
-            <input type="submit" value="Meld meg på" class="button">
-        </form>
-    </div>
+        <div class="columns large-12 small-6 box blog">
+            <h2>Blogg</h2>
+            <ul>
+                <?php foreach(FLorus::getPosts() as $post) { ?>
+                    <li>
+                        <a href="#"><?php echo $post['title']; ?></a>
+                        <span class="date"><?php echo $post['date']; ?></span>
+                    </li>
+                <?php } ?>
+            </ul>
+        </div>
 
-    <div class="box login">
-        <h2>Logg inn</h2>
-        <p>Velkommen, Mathias</p>
-    </div>
+        <div class="columns large-12 small-6 box newsletter">
+            <h2>Nyhetsbrev</h2>
+            <form action="">
+                <input type="email" placeholder="blomster@finn.no">
+                <input type="submit" value="Meld meg på" class="button">
+            </form>
+        </div>
+
+        <div class="columns large-12 small-6 box login">
+            <h2>Logg inn</h2>
+            <p>Velkommen, Mathias</p>
+        </div>
 
 </aside>
