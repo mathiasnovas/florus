ad92997460cc915ec2f82b8d82faa0322015532c
Mathias Novas <mathias@07.no>
2013-06-28 15:28:15 +0200

    Add possibility to register as costumer

--- tpl/parts/purchase/stage2.php
+++ tpl/parts/purchase/stage2.php
@@ -11,7 +11,20 @@
 
 <label for="delivery_time">Ønsket leveringstidspunkt</label>
 <input class="datepicker" type="date" id="delivery_time" name="delivery_time">
-<p class="info-box">Hvis ikke spesifisert, vil leveringstidspunkt være mellom 2-3 virkedager.</p>
+<p class="info-box">Hvis ikke spesifisert, vil leveringstidspunkt normalt være mellom 1-3 virkedager. Bestillinger før kl. 12:00 leveres samme dag.</p>
 
 <label for="special_instructions">Spesielle instruksjoner for levering</label>
 <textarea name="special_instructions" id="special_instructions" cols="30" rows="10"></textarea>
+
+<div class="separator"><i></i></div>
+
+<label for="name">Navn</label>
+<input name="name" id="name" type="text">
+
+<label for="email">E-post</label>
+<input type="email" id="email" name="email">
+
+<div>
+<label for="register">Registrer deg som kunde?</label>
+<input type="checkbox" id="register" name="register">
+</div>
