3b284ee650369a92628690728151f118c5de0044
Mathias Novas <mathias@07.no>
2013-06-28 13:24:16 +0200

    Add blog post template

---
+++ tpl/post.php
@@ -0,0 +1,13 @@
+<div class="page row">
+    <section class="columns large-12">
+        <article class="post">
+            <h1>Lorem ipsum dolor sit amet</h1>
+            <div class="info-box">Skrevet av: Herr Blomst - 06.06.2013</div>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget placerat sem. Suspendisse velit lectus, molestie id dignissim nec, sollicitudin vitae tortor. Duis lorem urna, gravida laoreet dignissim sit amet, aliquet a magna. Suspendisse potenti. Morbi quis dictum eros. Phasellus in sagittis eros. Nulla ultricies, nulla quis egestas venenatis, ipsum odio egestas nisl, a vehicula sapien urna non eros. Maecenas a mi nunc, sed euismod nisl. Vestibulum orci justo, fringilla eu accumsan non, ultricies vel mauris. Pellentesque malesuada, purus et ullamcorper tincidunt, augue tortor feugiat turpis, sed semper orci urna non neque. Maecenas imperdiet, erat vitae accumsan dapibus, orci massa porttitor velit, non tempus arcu lectus at arcu. Duis pharetra mollis tellus id molestie. Ut eu felis non leo mollis pretium.</p>
+
+            <p>Nunc porta, neque gravida malesuada volutpat, enim diam posuere massa, nec mollis ipsum diam eu nunc. Morbi ac iaculis orci. Cras libero metus, iaculis a lobortis ac, pulvinar sit amet risus. Aliquam sit amet est tincidunt dui aliquam lacinia. Integer tempus, enim eget tincidunt aliquet, nisl dolor volutpat lectus, vitae lacinia magna elit a leo. Integer lacus felis, ultricies in pellentesque et, pellentesque vel tellus. Pellentesque vehicula libero et lectus luctus ac tincidunt neque molestie. Sed a nisi nisl. Sed id lacinia orci.
+            </p>
+        </article>
+    </section>
+</div>
