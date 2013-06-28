573fca2115de0b3819f33a9a179919fa7062292c
Mathias Novas <mathias@07.no>
2013-06-28 14:46:06 +0200

    Make sure buttons in cart look alike

--- tpl/cart.php
+++ tpl/cart.php
@@ -3,20 +3,17 @@
     <section class="columns large-12">
         <h1>Handlevogn</h1>
 
-        <form action="/">
-            <input type="hidden" name="p" value="purchase">
-            <div class="products-list row">
-                <?php foreach (Florus::getProducts(3) as $product) { ?>
-                    <?php include 'tpl/parts/list_product.php'; ?>
-                <?php } ?>
-                <div class="sum columns large-12"></div>
-            </div>
+        <div class="products-list row">
+            <?php foreach (Florus::getProducts(3) as $product) { ?>
+                <?php include 'tpl/parts/list_product.php'; ?>
+            <?php } ?>
+            <div class="sum columns large-12"></div>
+        </div>
 
-            <div class="actions">
-                <div class="button remove all">Tøm handlekurven</div>
-                <input type="submit" class="button" value="Kjøp alle">
-            </div>
-        </form>
+        <div class="actions">
+            <div class="button remove all">Tøm handlekurven</div>
+            <div class="button"><a href="/?p=purchase">Kjøp alle</div>
+        </div>
     </section>
 
 </div>
