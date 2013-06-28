4b44aa05dd139ee607b9157f462d4b2a384a1688
Mathias Novas <mathias@07.no>
2013-06-28 14:46:47 +0200

    Typo in form

--- tpl/parts/purchase/stage2.php
+++ tpl/parts/purchase/stage2.php
@@ -9,7 +9,7 @@
 <label class="required" for="card_text">Tekst på kortet</label>
 <textarea name="" id="card_text" cols="30" rows="10"></textarea>
 
-<label for="delivery_time">Ønsket Leveringsditspunkt</label>
+<label for="delivery_time">Ønsket leveringstidspunkt</label>
 <input class="datepicker" type="date" id="delivery_time" name="delivery_time">
 <p class="info-box">Hvis ikke spesifisert, vil leveringstidspunkt være mellom 2-3 virkedager.</p>
 
